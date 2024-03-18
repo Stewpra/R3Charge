5211242eac2e3aed3dc6ad864daa1dbbada52aff
Stewpra <wilbstewart7@gmail.com>
2024-03-18 14:52:03 -0400

    added start timer function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
-
 function storeDataInLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
 
-function init() {}
+function startTimer(seconds) {
+  let timer = seconds;
+
+  const interval = setInterval(() => {
+    console.log(timer);
+
+    if (timer === 0) {
+      clearInterval(interval);
+      console.log("Countdown complete!");
+    } else {
+      timer--;
+    }
+  }, 1000);
+}
+
+function init() {
+  startTimer(0);
+}
 
 init();
-
