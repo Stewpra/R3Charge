8819de9e7ffa8ec75077ba17ac35da2ff4788aca
Stewpra <wilbstewart7@gmail.com>
2024-03-19 11:18:43 -0400

    getJoke Function

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,66 @@
-const timerButton = document.querySelector("#timer-button");
-const batteryBar = document.querySelector("#battery-bar");
+const timerButton = document.querySelector('#timer-button');
+const batteryBar = document.querySelector('#battery-bar');
 
 function storeDataInLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+	localStorage.setItem(key, JSON.stringify(data));
 }
 
 function startTimer(seconds) {
-  let timer = seconds;
+	let timer = seconds;
 
-  const interval = setInterval(() => {
-    console.log(timer);
+	const interval = setInterval(() => {
+		console.log(timer);
 
-    if (timer === 0) {
-      clearInterval(interval);
-      console.log("Countdown complete!");
-    } else {
-      updateBattery(timer, 30);
-      timer--;
-    }
-  }, 1000);
+		if (timer === 0) {
+			clearInterval(interval);
+			console.log('Countdown complete!');
+		} else {
+			updateBattery(timer, 30);
+			timer--;
+		}
+	}, 1000);
 }
 
 function updateBattery(time, totalTime) {
-  const percentage = (time / totalTime) * 100;
-  console.log(percentage);
-  batteryBar.style.width = `${percentage}%`;
+	const percentage = (time / totalTime) * 100;
+	console.log(percentage);
+	batteryBar.style.width = `${percentage}%`;
 }
 
 function fetchData(url) {
-  fetch(url, {
-    headers: {
-      Accept: "text/plain",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response error");
-      }
-      return response.text();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch:", error);
-    });
+	fetch(url, {
+		headers: {
+			Accept: 'text/plain',
+		},
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error('Network response error');
+			}
+			return response.text();
+		})
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((error) => {
+			console.error('There was a problem with the fetch:', error);
+		});
 }
 
-// Fetch a random dad joke
-fetchData("https://icanhazdadjoke.com/");
+function getJoke() {
+	const jokeUrl = 'https://icanhazdadjoke.com/';
+
+	fetchData(jokeUrl);
+}
 
 function handleClick() {
-  console.log("test click");
+	console.log('test click');
 }
 
-timerButton.addEventListener("click", handleClick);
+timerButton.addEventListener('click', handleClick);
 
 function init() {
-  startTimer(30);
+	startTimer(30);
 }
 
 init();
