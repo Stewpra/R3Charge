3e9d5ce003161691a82720ea9df127179da42ba8
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-20 15:52:19 -0400

    applying styles to modal content

--- assets/showText.js
+++ assets/showText.js
@@ -24,13 +24,13 @@ async function displayText(type) {
   console.log(`invalid parameter: ${type}`);
 }
 
-function displayModal(text) {
+function displayModal(title, text) {
   const modal = document.getElementById('my_modal_1');
   const modalContent = modal.querySelector('.modal-box');
 
   modalContent.innerHTML = `
-    <h3 class="font-bold text-lg">Hello</h3>
-    <p class="py-4">${text}</p>
+    <h3 class="text-center font-semibold">${title}</h3>
+    <p class="py-4 text-center text-4xl font-bold">${text}</p>
     <div class="modal-action">
       <form method="dialog">
         <button class="btn">Snooze!</button>
