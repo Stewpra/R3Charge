2766cb466205b4e237fc96d4b40dec024fd24aa4
Stewpra <wilbstewart7@gmail.com>
2024-03-20 11:02:58 -0400

    added startRecharge function

--- assets/script.js
+++ assets/script.js
@@ -4,33 +4,33 @@ const batteryDisplay = document.querySelector('#battery-display');
 const batteryBar = document.querySelector('#battery-bar');
 
 async function fetchData(url) {
-  let data;
-  try {
-    const res = await fetch(url, {
-      headers: {
-        accept: 'application/json',
-      },
-    });
-    data = await res.json();
-  } catch (err) {
-    console.error(err);
-  } finally {
-    return data;
-  }
+	let data;
+	try {
+		const res = await fetch(url, {
+			headers: {
+				accept: 'application/json',
+			},
+		});
+		data = await res.json();
+	} catch (err) {
+		console.error(err);
+	} finally {
+		return data;
+	}
 }
 
 function handleClick() {
-  timerButton.setAttribute('disabled', 'disabled');
-  startTimer(30);
+	timerButton.setAttribute('disabled', 'disabled');
+	startTimer(30);
 }
 
 function endWorkTime() {
-  hideBatteryDisplay();
-  displayText('quote');
+	hideBatteryDisplay();
+	displayText('quote');
 }
 
 function init() {
-  timerButton.addEventListener('click', handleClick);
+	timerButton.addEventListener('click', handleClick);
 }
 
 init();
