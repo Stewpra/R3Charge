7871f92adf8729c10920fce878794b6624d4b19d
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-19 20:58:02 -0400

    displayText function takes in either joke or quote as an argument to display either one to the page

--- assets/showText.js
+++ assets/showText.js
@@ -2,16 +2,26 @@ async function getJoke() {
   const jokeUrl = 'https://icanhazdadjoke.com/';
   const data = await fetchData(jokeUrl);
   const str = data.joke;
-  console.log(str);
+  return str;
 }
 
 async function getQuote() {
   const quoteUrl = 'https://api.quotable.io/random';
   const data = await fetchData(quoteUrl);
+  const str = data.content;
+  return str;
 }
 
-function displayText(str) {
-  textDisplay.innerText = str;
+async function displayText(type) {
+  if (type === 'joke') {
+    textDisplay.innerText = await getJoke();
+    return;
+  }
+  if (type === 'quote') {
+    textDisplay.innerText = await getQuote();
+    return;
+  }
+  console.log(`invalid parameter: ${type}`);
 }
 
-getJoke();
+displayText('joke');
