0688efc53a612f0e02c4c8ea3ca1bdafbe0560ee
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-19 20:52:21 -0400

    adjusted getJoke function to get the string from data

--- assets/showText.js
+++ assets/showText.js
@@ -1,17 +1,17 @@
 async function getJoke() {
   const jokeUrl = 'https://icanhazdadjoke.com/';
   const data = await fetchData(jokeUrl);
-  console.log(data);
-  return data;
+  const str = data.joke;
+  console.log(str);
 }
 
 async function getQuote() {
   const quoteUrl = 'https://api.quotable.io/random';
   const data = await fetchData(quoteUrl);
-  console.log(data);
-  return data;
 }
 
 function displayText(str) {
   textDisplay.innerText = str;
 }
+
+getJoke();
