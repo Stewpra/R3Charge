6dc2e63c1807b8854d65da51a46e6dba6b2b124f
Gary Bright <garybright@rocketmail.com>
2024-03-20 14:02:55 -0400

    adjusted the rest of the js file to reflect new variable. confirm syntax error cleared

--- assets/batteryDisplay.js
+++ assets/batteryDisplay.js
@@ -33,11 +33,11 @@ function getTimeUnits(time) {
 
 function updateTime(totalHours, totalMinutes, totalSeconds) {
   if (totalHours !== 0) {
-    displayText.textContent = `${totalHours} hours`;
+    displayTextEl.textContent = `${totalHours} hours`;
   } else if (totalMinutes !== 0) {
-    displayText.textContent = `${totalMinutes} minutes`;
+    displayTextEl.textContent = `${totalMinutes} minutes`;
   } else {
-    displayText.textContent = `${totalSeconds} seconds`;
+    displayTextEl.textContent = `${totalSeconds} seconds`;
   }
 }
 
