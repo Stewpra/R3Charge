c3b6d8d2e7b9ca460a4e099f568c9d6bc4ee2c21
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-20 14:04:34 -0400

    adjusted getTimeUnits function to get only minutes and seconds

--- assets/batteryDisplay.js
+++ assets/batteryDisplay.js
@@ -1,64 +1,48 @@
 const displayText = document.querySelector('#displayTime');
 
-function getTimeUnits(time) {
-	function getHours(totalSeconds) {
-		let hours = Math.floor(totalSeconds / 3600);
-		return hours;
-	}
+function getTimeUnits(totalSeconds) {
+  function getMinutes(seconds) {
+    let minutes = Math.floor(totalSeconds / 60);
+    return minutes;
+  }
 
-	function moduloFromHour(totalSeconds) {
-		let modulo = totalSeconds % 3600;
-		return modulo;
-	}
+  function moduloFromMinutes(seconds) {
+    let modulo = totalSeconds % 60;
+    return modulo;
+  }
 
-	function getMinutes(totalSeconds) {
-		let minutes = Math.floor(totalSeconds / 60);
-		return minutes;
-	}
+  const totalMinutes = getMinutes(totalSeconds);
+  const totalSeconds = moduloFromMinutes(totalSeconds);
 
-	function moduloFromMinutes(totalSeconds) {
-		let modulo = totalSeconds % 60;
-		return modulo;
-	}
-
-	const totalHours = getHours(time);
-	const remainingFromHours = moduloFromHour(time);
-
-	const totalMinutes = getMinutes(remainingFromHours);
-	const remainingFromMinutes = moduloFromMinutes(remainingFromHours);
-
-	const totalSeconds = remainingFromMinutes;
-	return { totalHours, totalMinutes, totalSeconds };
+  return { totalMinutes, totalSeconds };
 }
 
-function updateTime(totalHours, totalMinutes, totalSeconds){
-    if(totalHours !== 0){
-      displayText.textContent = `${totalHours} hours`;
-    } else if (totalMinutes !== 0){
-      displayText.textContent = `${totalMinutes} minutes`;
-    }else {
-      displayText.textContent = `${totalSeconds} seconds`;
-    }
+function updateTime(totalHours, totalMinutes, totalSeconds) {
+  if (totalHours !== 0) {
+    displayText.textContent = `${totalHours} hours`;
+  } else if (totalMinutes !== 0) {
+    displayText.textContent = `${totalMinutes} minutes`;
+  } else {
+    displayText.textContent = `${totalSeconds} seconds`;
+  }
 }
-
-updateTime(0, 55, 0);
 
 function updateBattery(time, totalTime) {
-	const percentage = (time / totalTime) * 100;
-	batteryBar.style.width = `${percentage}%`;
-	if (percentage >= 10) {
-		batteryBar.style.height = '100%';
-	}
-	if (percentage < 10) {
-		height = 15 * percentage;
-		batteryBar.style.height = `${height}px`;
-	}
-	if (percentage < 8) {
-		height = '105px';
-		batteryBar.style.height = height;
-	}
+  const percentage = (time / totalTime) * 100;
+  batteryBar.style.width = `${percentage}%`;
+  if (percentage >= 10) {
+    batteryBar.style.height = '100%';
+  }
+  if (percentage < 10) {
+    height = 15 * percentage;
+    batteryBar.style.height = `${height}px`;
+  }
+  if (percentage < 8) {
+    height = '105px';
+    batteryBar.style.height = height;
+  }
 }
 
 function hideBatteryDisplay() {
-	batteryDisplay.style.display = 'none';
+  batteryDisplay.style.display = 'none';
 }
