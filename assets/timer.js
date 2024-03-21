da60b6864f492434567a496d0a02056a98c898ff
Gary Bright <garybright@rocketmail.com>
2024-03-20 23:39:26 -0400

    added updateRemainingTimeDisplay to recharge function to fix display staying at 1

--- assets/timer.js
+++ assets/timer.js
@@ -30,6 +30,7 @@ async function startRecharge(seconds) {
       displayModal("Here's a joke to get you pumped up to work:", text);
       endRechargeTime();
     } else {
+      updateRemainingTimeDisplay(getTimeUnits(timer));
       updateBattery(timer, 30);
       timer++;
     }
